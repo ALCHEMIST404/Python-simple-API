d5eec386a7276e015679f931ba10b933b6a47081
Irina Sokolova <83919776+ALCHEMIST404@users.noreply.github.com>
2021-08-29 22:55:03 +0200

    Add files via upload

--- Client/client.py
+++ Client/client.py
@@ -11,18 +11,19 @@ import base64
 from client_test import client_test
 
 CLIENT_FOLDER = "Client/" #Folder where client files are located
-CLIENT_PREFIX = "http://192.168.1.3:5000/"
+CLIENT_PREFIX = "http://192.168.1.3:5000/" # Server IP
 
 def main():
     
     test = client_test(CLIENT_FOLDER, CLIENT_PREFIX) #Test method class for functional testing
     
-    
     #сalling test methods
-    test.test_get_photo() # get a photo from the server
-    test.test_system_state() #State of the system
+    test.test_get_state_recognition() #Get data from the program
+    test.test_get_screenshot() # Get a photo from the camera, processed by the recognition program
+    test.test_system_state() # State of the system
     test.test_BD() #Database testing
     test.test_files_transfer_to_server() #File upload testing
+    
     
 if __name__ == "__main__":
     main()    
